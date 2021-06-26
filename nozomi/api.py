afd7dc4dcd5b2716e062ce9a0041b08b640f6523
yusa-imit <esimileiar81@gmail.com>
2021-06-26 18:20:06 +0900

    add history feature

--- nozomi/api.py
+++ nozomi/api.py
@@ -1,5 +1,5 @@
 """Web API functions."""
-
+import json
 
 import logging
 import struct
@@ -42,6 +42,51 @@ def get_post(url: str) -> Post:
         _LOGGER.exception(ex)
         raise
 
+def getPostNumber(list:list):
+    new_list = []
+    for member in list:
+        new_list.append(member.split('/')[-1].split('.')[0])
+    return new_list
+
+def init_json(directory:str):
+    if not os.path.isfile(directory):
+        Path(directory).touch()
+        with open(directory, 'w') as f:
+            json.dump({"history":{}}, f, indent='\t')
+
+def readHistory(positive_tags: List[str], negative_tags: List[str]=None) -> list:
+    history_name = os.path.join(Path.cwd(), "history.json")
+    init_json(history_name)
+    with open(history_name,'r') as f:
+        try:
+            history = json.load(f)
+            if not "history" in history:
+                print("Not Correct History File, Delete history.json Automatically")
+                os.remove(history_name)
+                init_json(history_name)
+                return []
+        except:
+            print("History JSON Parse Error, Delete history.json")
+            os.remove(history_name)
+            init_json(history_name)
+            return []
+    tag = '+'.join(positive_tags)+'_'+'-'.join(negative_tags)
+    if not tag in history["history"]:
+        return []
+    return history["history"][tag]
+
+def saveHistory(add_history, positive_tags: List[str], negative_tags: List[str]=None, ):
+    history_name = os.path.join(Path.cwd(), "history.json")
+    with open(history_name,'r') as f:
+        history = json.load(f)
+    tag = '+'.join(positive_tags)+'_'+'-'.join(negative_tags)
+    if not tag in history["history"]:
+        history["history"][tag] = add_history
+    else:
+        history["history"][tag] = history["history"][tag] + add_history
+    with open(history_name,'w', encoding='utf-8') as f:
+        json.dump(history, f, indent="\t")
+
 
 def get_posts(positive_tags: List[str], negative_tags: List[str]=None) -> Iterable[Post]:
     """Retrieve all post data that contains and doesn't contain certain tags.
@@ -63,7 +108,10 @@ def get_posts(positive_tags: List[str], negative_tags: List[str]=None) -> Iterab
         positive_post_urls = _get_post_urls(positive_tags)
         negative_post_urls = _get_post_urls(negative_tags)
         relevant_post_urls = set(positive_post_urls) - set(negative_post_urls)
-        for post_url in relevant_post_urls:
+        tag_history = readHistory(positive_tags, negative_tags)
+        recent_history = relevant_post_urls - set(tag_history)
+        saveHistory(list(recent_history), positive_tags, negative_tags)
+        for post_url in recent_history:
             post_data = requests.get(post_url).json()
             _LOGGER.debug(post_data)
             yield from_dict(data_class=Post, data=post_data)
